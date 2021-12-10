43715999550efc50ce4fa6228008c5a493107ead
Ivan Minchev <Minchev1988@gmail.com>
2021-12-10 16:37:47 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/06.CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math.r
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = double.Parse(Console.ReadLine());
+            var P = double.Parse("3.14159265359");
+
+            var area = P * r * r;
+            var perimeter = 2 * P * r;
+            Console.WriteLine("Area =" + area);
+            Console.WriteLine("Perimeter = " + perimeter);
+
+        }
+    }
+}
