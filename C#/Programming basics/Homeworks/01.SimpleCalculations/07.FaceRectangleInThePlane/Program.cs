f69d200f1eb4c1b027a2b1830d8025f446fca71c
Ivan Minchev <Minchev1988@gmail.com>
2021-12-11 09:54:24 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/07.FaceRectangleInThePlane/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faceRectangleInThePlane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+
+            var a = x1 - x2;
+            var b = y2 - y1;
+
+            double positiveB = Math.Abs(b);
+            double positiveA = Math.Abs(a);
+
+            var face = positiveA * positiveB;
+            var perimeter = 2 * (positiveA + positiveB);
+
+            
+            Console.WriteLine("Face = " + face);
+            Console.WriteLine("Perimeter = " + perimeter);
+        }
+    }
+}
---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/07.FaceRectangleInThePlane/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("faceRectangleInThePlane")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("faceRectangleInThePlane")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97ad87f8-599a-40c9-94d1-00ba8cb2c5bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
