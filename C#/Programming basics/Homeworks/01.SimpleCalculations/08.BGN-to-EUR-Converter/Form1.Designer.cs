04c07d53fba08b0cfadba39f09fa2f03b1055b17
Ivan Minchev <Minchev1988@gmail.com>
2021-12-11 11:35:45 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/08.BGN-to-EUR-Converter/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BGN_to_EUR_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valueUpDown = new System.Windows.Forms.NumericUpDown();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.valueUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // valueUpDown
+            // 
+            this.valueUpDown.Location = new System.Drawing.Point(167, 25);
+            this.valueUpDown.Name = "valueUpDown";
+            this.valueUpDown.Size = new System.Drawing.Size(120, 20);
+            this.valueUpDown.TabIndex = 0;
+            this.valueUpDown.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.AutoSize = true;
+            this.valueLabel.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.valueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.valueLabel.Location = new System.Drawing.Point(125, 93);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(197, 24);
+            this.valueLabel.TabIndex = 1;
+            this.valueLabel.Text = "Insert value to convert!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Convert";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BGN to EUR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 153);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valueUpDown);
+            this.Controls.Add(this.valueLabel);
+            this.Name = "Form1";
+            this.Text = "BGN o EUR";
+            ((System.ComponentModel.ISupportInitialize)(this.valueUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown valueUpDown;
+        private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/08.BGN-to-EUR-Converter/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGN_to_EUR_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            var value = valueUpDown.Value;
+            valueLabel.Text = (value / 1.95583M).ToString();
+        }
+    }
+}
