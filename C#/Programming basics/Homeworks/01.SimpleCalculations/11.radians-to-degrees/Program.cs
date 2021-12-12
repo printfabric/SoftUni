1ed35cfc551285f0d13c556d9f27c36b4695e87a
Ivan Minchev <Minchev1988@gmail.com>
2021-12-12 08:36:58 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/11.radians-to-degrees/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radians_to_degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = double.Parse(Console.ReadLine());
+            // var h = double.Parse(Console.ReadLine());
+
+
+            var F = (180.0 / Math.PI) * d;
+            double FRound = Math.Round(F, 2);
+
+            Console.WriteLine("fahrenheit  = " + FRound);
+
+        }
+    }
+}
