807425cc792ddf3390aa30149315de775f339202
Ivan Minchev <Minchev1988@gmail.com>
2021-12-12 10:59:46 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/12.USD-to-BGN/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.USDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn =Math.Round(usd * 1.82585039);
+            double mathRound = Math.Round(bgn, 2);
+            Console.WriteLine(mathRound + " BGN");
+
+        }
+    }
+}
