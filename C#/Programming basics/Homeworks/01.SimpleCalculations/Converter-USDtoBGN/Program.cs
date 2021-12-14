5c329ee7b626c512e3e36211ef472add7bce9d0c
Ivan Minchev <Minchev1988@gmail.com>
2021-12-14 15:25:25 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/01.SimpleCalculations/Converter-USDtoBGN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_USDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.Write("USD = ");
+            var BGN = double.Parse(Console.ReadLine());            
+            var USD = BGN * 1.79549;
+            double USDRound = Math.Round(USD, 2);
+          //  Console.Write("BGN = ");
+            Console.WriteLine(USDRound + " BGN");
+        }
+    }
+}
