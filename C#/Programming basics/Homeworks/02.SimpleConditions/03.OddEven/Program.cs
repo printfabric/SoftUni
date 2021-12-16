6fcaa2a718d0f95cd2beff9216eca935f110cf67
Ivan Minchev <Minchev1988@gmail.com>
2021-12-16 11:52:02 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/02.SimpleConditions/03.OddEven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double num = double.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+        }
+    }
+}
+
+
