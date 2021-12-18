69b6bd6a57a2e00d371ce65d6dd7a2c74bac7c46
Ivan Minchev <Minchev1988@gmail.com>
2021-12-18 17:05:02 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/02.SimpleConditions/05.GesThePass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ges_The_Pass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputParam = (Console.ReadLine());
+
+            if (inputParam == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
