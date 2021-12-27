e686963291f2345f551caf3910d45790c0eeb613
Ivan Minchev <Minchev1988@gmail.com>
2021-12-27 18:11:29 +0200

    Adding project files.

---
+++ C++/01.IntroductionToC++BasicSyntax/12.DepositCalculatior/12.DepositCalculatior.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double depositАmount;
+    int depositTerm;
+    double annualInterestRate;
+
+    cin >> depositАmount;
+    cin >> depositTerm;
+    cin >> annualInterestRate;
+
+    double sum = depositАmount + depositTerm * ((depositАmount * (annualInterestRate / 100)) / 12);
+    cout << sum;
+
+    return 0;
+}
