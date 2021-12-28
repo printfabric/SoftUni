169190e5a6100d67cd526ae5abf1752a066cd3e1
Ivan Minchev <Minchev1988@gmail.com>
2021-12-28 23:19:56 +0200

    Adding project files.

---
+++ C++/04.ForLoops/02.NumbersNto1/02.NumbersNto1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (size_t i = n; i > 0; i--)
+	{
+		cout << i << endl;
+	}
+
+}
---
+++ C++/04.ForLoops/03.Numbers1toN_WithStep3/03.Numbers1toN_WithStep3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (size_t i = 1; i <= n; i+=3)
+	{
+		cout << i << endl;
+	}
+}
