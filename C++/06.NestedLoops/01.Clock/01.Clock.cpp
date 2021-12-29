be4774978548486af1bd114a68e27e9a690ca0a4
Ivan Minchev <Minchev1988@gmail.com>
2021-12-29 21:40:57 +0200

    Adding Project files.

---
+++ C++/06.NestedLoops/01.Clock/01.Clock.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (size_t i = 0; i < 24; i++)
+	{
+		for (size_t j = 0; j < 60; j++)
+		{
+			cout << i << ":" << j << endl;
+		}
+	}
+}
