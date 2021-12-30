9442f7c9c8886a79d7eb90273420e6d2dfdb6782
Ivan Minchev <Minchev1988@gmail.com>
2021-12-30 15:15:04 +0200

    Adding project files.

---
+++ C++/06.NestedLoops/06.Building/06.Building.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int floors;
+	int rooms;
+
+	cin >> floors;
+	cin >> rooms;
+
+	for (size_t i = floors; i > 0; i--)
+	{
+		for (size_t j = 0; j < rooms; j++)
+		{
+			if (i == floors)
+			{
+				cout << "L" << i << j << " ";
+			}
+			else if (i % 2 == 0)
+			{
+				cout << "O" << i << j << " ";
+			}
+			else
+			{
+				cout << "A" << i << j << " ";
+			}
+		}
+
+		cout << endl;
+	}
+}
