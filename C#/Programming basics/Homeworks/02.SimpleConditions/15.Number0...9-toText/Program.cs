0e17d57fd3d0748aeef84dbb1eec40666209b16f
Ivan Minchev <Minchev1988@gmail.com>
2022-01-05 23:02:58 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/02.SimpleConditions/15.Number0...9-toText/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_0._._._9_toText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0 && n <= 9)
+            {
+                string[] arrOne = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+                Console.WriteLine(arrOne[n - 1]);
+            }
+            else
+            {
+                Console.WriteLine("number too big");
+            }
+        }
+    }
+}
