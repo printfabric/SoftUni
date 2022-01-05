199c216aa989c26ff3e39d5465b47b30e2e7f194
Ivan Minchev <Minchev1988@gmail.com>
2022-01-05 23:05:59 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/02.SimpleConditions/17.SameNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SameNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            int wordnumberOne = int.Parse(Console.ReadLine());
+            int wordnumberTwo = int.Parse(Console.ReadLine());
+            int wordnumberThree = int.Parse(Console.ReadLine());
+
+
+
+            if (wordnumberOne == wordnumberTwo)
+            {
+                if (wordnumberTwo == wordnumberThree)
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+                               
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
