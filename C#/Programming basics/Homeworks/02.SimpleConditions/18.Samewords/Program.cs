c989b3af490f140ab9c3cf78064f14aba392cc36
Ivan Minchev <Minchev1988@gmail.com>
2022-01-06 16:47:27 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/02.SimpleConditions/18.Samewords/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Same_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = (Console.ReadLine());
+            string wordTwo = (Console.ReadLine());
+
+            string lowerWord = word.ToLower();
+            string lowerWordTwo = wordTwo.ToLower();
+
+            if (lowerWord == lowerWordTwo)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+           
+             
+          
+
+
+        }
+    }
+}
