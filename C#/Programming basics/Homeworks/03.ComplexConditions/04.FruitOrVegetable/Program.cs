f11318a2b344b907662cb4b534426d443f23fd4c
Ivan Minchev <Minchev1988@gmail.com>
2022-01-28 11:17:22 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/03.ComplexConditions/04.FruitOrVegetable/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fv = Console.ReadLine();
+ 
+            if (fv == "banana")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (fv == "apple")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (fv == "kiwi")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (fv == "cherry")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (fv == "lemon")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (fv == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (fv == "tomato")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else if (fv == "cucumber")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else if (fv == "pepper")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else if (fv == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else 
+            {
+                Console.WriteLine("unknown");
+            }
+
+        }
+    }
+}
