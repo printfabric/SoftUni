90d2702d0f0b569835ce4992b8e9d44951de81dd
Ivan Minchev <Minchev1988@gmail.com>
2022-02-08 11:21:30 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/03.ComplexConditions/10.Cinema/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var Premiere = 12.00;
+            var normal = 7.5;
+            var Discount = 5;
+
+             */
+            string type = (Console.ReadLine());
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            
+
+            switch (type)
+            {
+                case "Premiere":
+                    Console.WriteLine("{0:f2}", 12.00 * (r * c));
+                    break;
+
+                case "Normal":
+                    Console.WriteLine("{0:f2}", 7.50 * (r * c));
+                    break;
+                case "Discount":
+                    Console.WriteLine("{0:f2}", 5.00 * (r * c));
+                    break;
+
+
+            }
+
+                        
+                    
+
+        }
+    }
+}
