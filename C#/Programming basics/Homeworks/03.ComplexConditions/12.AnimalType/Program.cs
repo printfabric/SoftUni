660c64e688f85cabafab0ca2b220520b8e212600
Ivan Minchev <Minchev1988@gmail.com>
2022-02-09 12:06:21 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/03.ComplexConditions/12.AnimalType/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animalType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string UserNumber = (Console.ReadLine());
+
+            switch (UserNumber)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":                
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+
+
+        }
+    }
+}
