5dcb808c374dec6c51b416bf487c913036023645
Ivan Minchev <Minchev1988@gmail.com>
2022-02-10 16:27:50 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/03.ComplexConditions/13.DayOfWeek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            if (day >= 1 && day <= 7)
+            {
+                string[] arr = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+                Console.WriteLine(arr[day - 1] + " ");    
+            }
+
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
+
+
