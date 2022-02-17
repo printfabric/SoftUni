f1249c62b2e4260da040861b62a8e8bae55e8cfc
Ivan Minchev <Minchev1988@gmail.com>
2022-02-17 12:41:48 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/04.Loops/07. ChristmasTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                var stars = new string('*', i);
+                var spaces = new string(' ', n - i);
+                Console.Write(spaces);
+                Console.Write(stars);
+                Console.Write(" | ");
+                Console.WriteLine(stars);
+                //Console.WriteLine(spaces);              
+
+            }
+            
+        }
+    }
+}
