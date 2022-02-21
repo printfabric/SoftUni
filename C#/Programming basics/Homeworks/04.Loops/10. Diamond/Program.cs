6e3e6ea2f1ef8bb4ecd7f1010b286b7d3f502e29
Ivan Minchev <Minchev1988@gmail.com>
2022-02-21 22:29:07 +0200

    Adding project files.

---
+++ C#/Programming basics/Homeworks/04.Loops/10. Diamond/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Diamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var leftRight = (n - 1) / 2;
+            var leftRightB = 0;
+
+            for (int i = 1; i <= (n-1) / 2; i++)
+            {
+                Console.Write(new string('-', leftRight));
+                Console.Write("*");
+                var mid = n - 2 * leftRight - 2;
+
+                if (mid >= 0)
+                {
+                    Console.Write(new string('-', mid));
+                    Console.Write("*");
+                }
+                Console.WriteLine(new string('-', leftRight));
+                leftRight--;
+                
+            }
+
+            for (int i = 1; i <= (n + 1) / 2; i++)
+            {
+                Console.Write(new string('-', leftRightB));
+                Console.Write("*");                
+                var mid = n - 2 * leftRightB - 2;
+
+                if (mid >= 0)
+                {
+                    Console.Write(new string('-', mid));
+                    Console.Write("*");
+                }
+                Console.WriteLine(new string('-', leftRightB));
+                leftRightB++;
+            }
+        }
+    }
+}
