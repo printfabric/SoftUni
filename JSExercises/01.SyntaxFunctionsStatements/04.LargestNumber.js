42d63da1ec26bb45d89da66469b1f33f453deb4a
Ivan Minchev <Minchev1988@gmail.com>
2022-03-21 10:04:02 +0200

    Adding project files.

--- JSExercises/01.SyntaxFunctionsStatements/04.LargestNumber.js
+++
@@ -1,6 +0,0 @@
-function solve(first, second, third) {
-    let maxNum = Math.max(first, second, third);
-    console.log(`The largest number is ${maxNum}.`);
-}
-
-solve(5, -3, 16);
--- JSExercises/02.Arrays/05.ProcessOddNumbers.js
+++
@@ -1,9 +0,0 @@
-function solve(arr) {
-    let result = arr.filter((x, i) => i % 2 != 0)
-    .map(x => x * 2)
-    .reverse();
-
-    console.log(result.join(' '));
-}
-
-solve([3, 0, 10, 4, 7, 3]);
