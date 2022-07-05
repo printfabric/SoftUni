70fd84fff86d3110e70f6c3798438aa5aafdc1d0
Ivan Minchev <Minchev1988@gmail.com>
2022-07-05 08:25:33 +0300

    Adding project files.

--- JS/01.SyntaxFunctionsStatements/01.StringLength.js
+++
@@ -1,9 +0,0 @@
-function solve(first, second, third) {
-    let sum = first.length + second.length + third.length;
-    let average = sum / 3;
-
-    console.log(sum);
-    console.log(Math.floor(average));
-}
-
-solve('chocolate', 'ice cream', 'cake');
