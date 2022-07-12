d8c73dd8c2f54d84c132be244efc2f216fa17a6a
Ivan Minchev <Minchev1988@gmail.com>
2022-07-12 07:59:19 +0300

    Adding project files.

--- JS/JSExercisesFolder/01.JavaScriptFundamentals/ArraysAdvanced/07.ListOfProducts.js
+++
@@ -1,8 +0,0 @@
-function solve(arr) {
-  arr.sort();
-  for (let i = 0; i < arr.length; i++) {
-    console.log(`${i + 1}.${arr[i]}`);
-  }
-}
-
-solve(['Potatoes', 'Tomatoes', 'Onions', 'Apples']);
