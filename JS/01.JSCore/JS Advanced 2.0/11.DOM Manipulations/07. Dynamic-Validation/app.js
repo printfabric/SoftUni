722383c026e37a79dcfa5ad02853ba900194181a
Ivan Minchev <Minchev1988@gmail.com>
2023-08-13 11:40:00 +0300

    Adding project files.

---
+++ JS/01.JSCore/JS Advanced 2.0/11.DOM Manipulations/07. Dynamic-Validation/app.js
@@ -0,0 +1,15 @@
+function validate() {
+    let inputElement = document.getElementById('email');
+
+    inputElement.addEventListener('change', e => {
+        let currentValue = e.currentTarget.value;
+
+        let pattern = /[a-z]+@[a-z]+.[a-z]+/;
+
+        if (!pattern.test(currentValue)) {
+            e.currentTarget.classList.add('error');
+        } else {
+            e.currentTarget.classList.remove('error');
+        }
+    });
+}
