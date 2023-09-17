4fb2c052130ca02efa3bcf44ceb459df015fc0ec
Ivan Minchev <Minchev1988@gmail.com>
2023-09-17 22:20:25 +0300

    Adding project files.

---
+++ JS/01.JSCore/JS Advanced 2.0/13.FunctionContext/01.AreaAndVolumeCalculator.js
@@ -0,0 +1,28 @@
+function area() {
+    return this.x * this.y;
+};
+
+function vol() {
+    return this.x * this.y * this.z;
+};
+
+function solve(area, vol, input) {
+    let figures = JSON.parse(input);
+
+    let result = figures.map(function(figure) {
+        return {
+            area: Math.abs(area.call(figure)),
+            volume: Math.abs(vol.call(figure))
+        }
+    });
+    
+    return result; 
+}
+
+let res = solve(area, vol, `[
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}
+    ]`);
+
+console.log(res);
