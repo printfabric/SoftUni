8cc339441124a69d5930e98cd991600260719492
Ivan Minchev <Minchev1988@gmail.com>
2024-01-09 20:41:43 +0200

    Adding project files.

---
+++ JS/01.JSCore/JS Advanced/04.DOM/DOMLab/01. Articles List/solution.js
@@ -0,0 +1,49 @@
+function createArticle() {
+    let getTitleElement = document.getElementById('createTitle');
+    let getContentElement = document.getElementById('createContent');
+
+    let headingElement = document.createElement('h3');
+    headingElement.innerHTML = getTitleElement.value;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerHTML = getContentElement.value;
+
+    let articleElement = document.createElement('article');
+    articleElement.appendChild(headingElement);
+    articleElement.appendChild(contentElement);
+
+    console.log(getTitleElement.value.length);
+
+    if (getTitleElement.value.length > 0 && getContentElement.value.length > 0)
+    {
+        let articleSectionElement = document.getElementById('articles');
+        articleSectionElement.appendChild(articleElement);
+    }
+} 
+
+
+// function createArticle() {
+//     let getTitleElement = document.getElementById('createTitle');
+//     let getContentElement = document.getElementById('createContent')
+
+//     let headingElement = document.createElement('h3');
+//     headingElement.innerHTML = getTitleElement.value;
+    
+//     let contentElement = document.createElement('p');
+//     contentElement.innerHTML = getContentElement.value;
+    
+//     let articleElement = document.createElement('article');
+//     articleElement.appendChild(headingElement);
+//     articleElement.appendChild(contentElement)
+    
+//     console.log(getTitleElement.value.length);
+    
+//     if (getTitleElement.value.length > 0 && getContentElement.value.length > 0)
+//     { 
+//         let articleSectionElement = document.getElementById('articles')
+//         articleSectionElement.appendChild(articleElement);    
+//     }  
+    
+//     getTitleElement.value = '';
+//     getContentElement.value = '';
+// }
